584cb6174a859b9218b2c8c74b33c29274a9de6e
Martin Koscak <martin95koscak@gmail.com>
2025-12-04 00:24:47 +0100

    Refactored Day 2, started to optimize.

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -21,7 +21,7 @@ impl crate::Advent for GiftShop {
         let mut invalid_ids: Vec<u128> = vec![];
         for range in &self.ranges {
             for num in range.0..=range.1 {
-                if Self::check_id_valid(num.to_string()) == false {
+                if Self::check_id_valid_01(num.to_string()) == false {
                     invalid_ids.push(num);
                 }
                 
@@ -33,11 +33,10 @@ impl crate::Advent for GiftShop {
     fn part_02(&self) -> String {
         let mut invalid_ids: Vec<u128> = vec![];
         for range in &self.ranges {
-            for num in range.0..=range.1 {
-                if Self::check2(num.to_string()) == false {
+            for num in range.0..=range.1 {                
+                if Self::check_id_valid_02(num.to_string()) == false {
                     invalid_ids.push(num);
-                }
-                
+                }                
             }            
         }
         invalid_ids.iter().sum::<u128>().to_string()
@@ -45,42 +44,38 @@ impl crate::Advent for GiftShop {
 }
 
 impl GiftShop {
-    fn check_id_valid(mut id: String) -> bool {
+    fn check_id_valid_01(id: String) -> bool {
         let odd = id.len() % 2 == 1;
         if odd {
             return true;
         }        
+
         let part_len = id.len() / 2;
-        let rhs = id.split_off(part_len);
-        id != rhs
+        let lhs = &id[0..part_len];
+        let rhs = &id[part_len..];        
+        lhs != rhs
     }
 
-    fn check2(id: String) -> bool {
-        let max_part_len = id.len() / 2;
-        'size_loop: for part_size in (1..=max_part_len).rev() {
-            if id.len() % part_size != 0 {
+    fn check_id_valid_02(id: String) -> bool { 
+        'part_length_loop: for part_len in 1..=id.len() / 2 {
+            if (id.len() % part_len) != 0 {
                 continue;
-            }    
-        
-            let mut current_min = 0;
-            let mut current_max = part_size;            
-            let mut lhs = &id[current_min..current_max];
-
-            current_min += part_size;
-            current_max += part_size; 
-            while current_max <= id.len() {                                
-                let rhs = &id[current_min..current_max];
-                if lhs != rhs {
-                    continue 'size_loop
+            }
+            let mut i = 0;
+            let pattern = &id[i..part_len];
+            i += part_len;                        
+            while i + part_len <= id.len() {
+                let next_pattern = &id[i..i + part_len];
+                if pattern != next_pattern {
+                    continue 'part_length_loop
                 }
-                current_min += part_size;
-                current_max += part_size;
-                lhs = rhs;
+                i += part_len;
             }
             return false;
         }
         true
     }
+
 }
 
 #[cfg(test)]
@@ -91,54 +86,77 @@ mod tests {
     fn test_num_valid_01() {
         for i in 11..=22 {
             if i == 11 || i == 22 {
-                assert_eq!(GiftShop::check_id_valid(i.to_string()), false);
+                assert_eq!(GiftShop::check_id_valid_01(i.to_string()), false);
             } else {
-                assert_eq!(GiftShop::check_id_valid(i.to_string()), true);
+                assert_eq!(GiftShop::check_id_valid_01(i.to_string()), true);
             }            
         }
 
         for i in 1188511880..=1188511890 {
             if i == 1188511885 {
-                assert_eq!(GiftShop::check_id_valid(i.to_string()), false);
+                assert_eq!(GiftShop::check_id_valid_01(i.to_string()), false);
             } else {
-                assert_eq!(GiftShop::check_id_valid(i.to_string()), true);
+                assert_eq!(GiftShop::check_id_valid_01(i.to_string()), true);
             }            
         }
-        assert_eq!(GiftShop::check_id_valid("123123".to_owned()), false);
-        assert_eq!(GiftShop::check_id_valid("133123".to_owned()), true);
+        assert_eq!(GiftShop::check_id_valid_01("123123".to_owned()), false);
+        assert_eq!(GiftShop::check_id_valid_01("133123".to_owned()), true);
 
-        assert_eq!(GiftShop::check_id_valid("101".to_owned()), true);        
+        assert_eq!(GiftShop::check_id_valid_01("101".to_owned()), true);        
     }
 
     #[test]
     fn test_num_valid_02() {
-        assert_eq!(GiftShop::check2("11".to_owned()), false); 
+        assert_eq!(GiftShop::check_id_valid_02("11".to_owned()), false);
+        assert_eq!(GiftShop::check_id_valid_02("12".to_owned()), true);
+        assert_eq!(GiftShop::check_id_valid_02("101".to_owned()), true);
+    }
+
+    #[test] 
+    fn test_range_valid_02() {
         for i in 11..=22 {
-            if i == 11 || i == 22 {
-                assert_eq!(GiftShop::check2(i.to_string()), false);
-            } else {
-                assert_eq!(GiftShop::check2(i.to_string()), true);
-            }            
+            let expected = ![11,22].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
         }
-
+        for i in 99..=115 {
+            let expected = ![99,111].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
         for i in 998..=1012 {
-            if i == 999 || i == 1010 {
-                assert_eq!(GiftShop::check2(i.to_string()), false);
-            } else {
-                assert_eq!(GiftShop::check2(i.to_string()), true);
-            }            
+            let expected = ![999,1010].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
         }
-
         for i in 1188511880..=1188511890 {
-            if i == 1188511885 {
-                assert_eq!(GiftShop::check2(i.to_string()), false);
-            } else {
-                assert_eq!(GiftShop::check2(i.to_string()), true);
-            }            
+            let expected = ![1188511885].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
         }
-        assert_eq!(GiftShop::check2("123123".to_owned()), false);
-        assert_eq!(GiftShop::check2("133123".to_owned()), true);
-
-        assert_eq!(GiftShop::check2("101".to_owned()), true);        
+        for i in 222220..=222224 {
+            let expected = ![222222].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
+        for i in 1698522..=1698528 {
+            let expected = ![].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
+        for i in 446443..=446449 {
+            let expected = ![446446].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
+        for i in 38593856..=38593862 {
+            let expected = ![38593859].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
+        for i in 565653..=565659 {
+            let expected = ![565656].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
+        for i in 824824821..=824824827 {
+            let expected = ![824824824].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected);  
+        }
+        for i in 2121212118..=2121212124 {
+            let expected = ![2121212121].contains(&i);
+            assert_eq!(GiftShop::check_id_valid_02(i.to_string()), expected, "at number {}", i);  
+        }
     }
 }
