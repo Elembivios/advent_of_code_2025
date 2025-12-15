344eafb398f0c2d31f1864a20cbc94c94d039051
Martin Koscak <martin95koscak@gmail.com>
2025-12-15 22:55:09 +0100

    Day 10 part 1

--- src/day_10/mod.rs
+++ src/day_10/mod.rs
@@ -1,6 +1,3 @@
-use owo_colors::OwoColorize;
-
-use crate::utils::wait_user_input;
 
 pub struct Factory {
     machines: Vec<Machine>
@@ -11,31 +8,40 @@ impl crate::Advent for Factory {
         where 
             Self: Sized {
         let machines: Vec<_> = data.lines().map(|l| {
-            let mut lights = vec![];
+            let mut lights = 0;
+            let mut lights_len = 0;
             let mut buttons = vec![];
             let mut joltages = vec![];
             for part in l.split(" ") {
                 if part.starts_with('[') {
-                    lights = part.chars().skip(1).take(part.len() - 2).map(|c| {
+                    for (i, c) in part.chars().skip(1).enumerate().take(part.len() - 2) {
                         if c == '#' {
-                            true
-                        } else {
-                            false
+                            lights |= 1 << i;
+                            lights_len = i;
                         }
-                    }).collect();
+                    }
                 } else if part.starts_with('(') {                    
                     let p = part[1..part.len() - 1].split(",").map(|c| c.parse::<usize>().unwrap()).collect::<Vec<usize>>();
-                    buttons.push(p);
+                    let mut bp: usize = 0;
+                    for bi in p {
+                        bp |= 1 << bi;
+                    }
+                    buttons.push(bp);
                 } else if part.starts_with('{') {
                     joltages = part[1..part.len() - 1].split(',').map(|c| c.parse::<usize>().unwrap()).collect();
                 }                
             }
+
+            println!("Lights: {}", lights);
+
             Machine {
                 lights,
+                lights_len,
                 buttons,
                 joltages
             }
         }).collect();
+
         Self { machines }
     }
 
@@ -45,8 +51,7 @@ impl crate::Advent for Factory {
         for (i, machine) in self.machines.iter().enumerate() {
             let mut min = usize::MAX;
             for button in &machine.buttons {
-                let lights = (0..machine.lights.len()).map(|_| false).collect();
-                let new_val = machine.step(lights, 0, button.clone(), min);
+                let new_val = machine.step(0, 0, button.clone(), min);
                 if new_val < min {
                     min = new_val;
                 }
@@ -59,18 +64,23 @@ impl crate::Advent for Factory {
     }
 
     fn part_02(&self) -> String {
+        for machine in &self.machines {
+            let joltage_counters: Vec<usize> = vec![0; machine.lights_len];
+            
+        }
         2.to_string()
     }
 }
 
 pub struct Machine {
-    lights: Vec<bool>,
-    buttons: Vec<Vec<usize>>,
+    lights: usize,
+    lights_len: usize,
+    buttons: Vec<usize>,
     joltages: Vec<usize>
 }
 
 impl Machine {
-    pub fn step(&self, lights: Vec<bool>, counter: usize, button: Vec<usize>, previous_min: usize) -> usize {        
+    pub fn step(&self, lights: usize, counter: usize, button: usize, previous_min: usize) -> usize {        
         let mut lights = lights;
         let mut counter = counter;
         counter += 1;
@@ -80,9 +90,9 @@ impl Machine {
         }
         if counter > previous_min || counter > self.buttons.len() {
             return counter;
-        }         
+        }
         
-        let mut min_counter = usize::MAX;
+        let mut min_counter = previous_min;
         for bw in &self.buttons {
             if *bw == button {
                 continue;
@@ -96,10 +106,7 @@ impl Machine {
         min_counter
     }
 
-    pub fn switch(lights: &mut Vec<bool>, bw: &Vec<usize>) {
-        for b in bw {
-            let light = lights.get_mut(*b).unwrap();
-            *light = !*light;
-        }
+    pub fn switch(lights: &mut usize, bw: &usize) {
+        *lights ^= bw;
     }
 }
